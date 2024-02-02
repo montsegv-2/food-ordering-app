011c03a3c0739355035d32a7d90cfe5c233c5875
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-02-02 17:18:07 -0600

    Add buttons order now and learn more

--- src/components/icons/Right.js
+++ src/components/icons/Right.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Right({ className = "w-6 h-6  " }) {
+export default function Right({ className = "w-6 h-6" }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -4,21 +4,25 @@ import Right from "../icons/Right";
 export default function Hero() {
   return (
     <>
-      <section className="grid grid-cols-2">
-        <div>
+      <section className="hero">
+        <div className="py-12">
           <h1 className="text-4xl font-semibold">
-            Everything is better with pizza{" "}
+            Everything is better with&nbsp;
+            <span className="text-primary">Pizza</span>
           </h1>
-          <p className="mt-4 my-4 text-gray-500">
+          <p className="my-4 text-gray-500">
             Pizza is the missing piece that makes every day complete, a simple
             yet delicious joy in life
           </p>
-          <div className="flex gap-4">
-            <button className="bg-primary text-white px-8 py-2 rounded-full">
+          <div className="flex gap-4 text-sm">
+            <button className="bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
               Order now
               <Right />
             </button>
-            <button>Learn more</button>
+            <button className="flex gap-2 py-2 text-gray-600 font-semibold">
+              Learn more
+              <Right />
+            </button>
           </div>
         </div>
         <div className="relative">
