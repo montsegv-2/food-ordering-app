1e783e8f72e45161ca42fc625b451935b8d24013
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-02-02 17:57:44 -0600

    Add HomeMenu component

---
+++ src/components/layout/HomeMenu.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function HomeMenu() {
+  return (
+    <>
+      <section className="text-center">
+        <div className="text-center">
+          <h3 className="uppercase text-gray-600 font-semibold leading-4">
+            Check out
+          </h3>
+          <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+        </div>
+      </section>
+    </>
+  );
+}
