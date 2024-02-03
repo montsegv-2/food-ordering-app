c996c3431cb926e9e8b9b4bf42b59426d0c6dd3d
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-02-03 01:46:09 -0600

    fix: complete homeMenu

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -1,28 +1,19 @@
+import Image from "next/image";
 import React from "react";
 
 export default function HomeMenu() {
   return (
     <section className="">
-      <div className="absolute left-0 right-0">
-        <div className="h-48 w-48 absolute left-0">
-          <Image
-            src={"/sallad1.png"}
-            layout={"fill"}
-            objectFit={"contain"}
-            alt={"sallad"}
-          />
+      <div className="absolute h-full left-0 right-0 w-full justify-start">
+        <div className="absolute left-0 -top-[70px] text-left">
+          <Image src={"/sallad1.png"} width={109} height={189} alt={"sallad"} />
         </div>
-        <div className="h-48 w-48 absolute right-0">
-          <Image
-            src={"/sallad2.png"}
-            layout={"fill"}
-            objectFit={"contain"}
-            alt={"sallad"}
-          />
+        <div className="absolute -top-[300px] right-0 -z-10">
+          <Image src={"/sallad2.png"} width={107} height={195} alt={"sallad"} />
         </div>
       </div>
       <div className="text-center">
-        <h3 className="uppercase text-gray-600 font-semibold leading-4">
+        <h3 className="uppercase text-gray-500 font-semibold leading-4">
           Check out
         </h3>
         <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
