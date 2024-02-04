dde5cbcaba4a93b889d7efc15e9c947ecc52cad4
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-02-04 01:20:43 -0600

    Add effects in the menuItems

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import React from "react";
+import MenuItem from "../menu/menuItem";
 
 export default function HomeMenu() {
   return (
     <section className="">
-      <div className="absolute h-full left-0 right-0 w-full justify-start">
+      <div className="absolute left-0 right-0 w-full justify-start">
         <div className="absolute left-0 -top-[70px] text-left">
           <Image src={"/sallad1.png"} width={109} height={189} alt={"sallad"} />
         </div>
@@ -12,24 +12,19 @@ export default function HomeMenu() {
           <Image src={"/sallad2.png"} width={107} height={195} alt={"sallad"} />
         </div>
       </div>
-      <div className="text-center">
+      <div className="text-center mb-4">
         <h3 className="uppercase text-gray-500 font-semibold leading-4">
           Check out
         </h3>
         <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
       </div>
       <div className="grid grid-cols-3 gap-4">
-        <div className="bg-gray-200 p-4 rounded-lg text-center">
-          <img src="/pizza.png" alt="pizza" />
-          <h4 className="font-semibold text-xl my-3">Pepperoni Pizza</h4>
-          <p className="text-gray-500 text-sm">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
-          </p>
-          <button className="mt-4 bg-primary text-white rounded-full px-8 py-2">
-            Add to car $12
-          </button>
-        </div>
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
       </div>
     </section>
   );
--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -1,5 +1,15 @@
-import React from "react";
-
-export const menuItem = () => {
-  return <div>menuItem</div>;
-};
+export default function MenuItem() {
+  return (
+    <div className="bg-gray-200 p-4 rounded-lg text-center hover:bg-white transition-all">
+      <img src="/pizza.png" alt="pizza" />
+      <h4 className="font-semibold text-xl my-3">Pepperoni Pizza</h4>
+      <p className="text-gray-500 text-sm">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry.
+      </p>
+      <button className="mt-4 bg-primary text-white rounded-full px-8 py-2">
+        Add to car $12
+      </button>
+    </div>
+  );
+}
