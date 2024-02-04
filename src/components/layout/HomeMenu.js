904ea4a574d3a8ea01fd4fd2e0f7bfd70c5b0def
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-02-04 01:57:05 -0600

    Fix cards in the body page

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -5,10 +5,10 @@ export default function HomeMenu() {
   return (
     <section className="">
       <div className="absolute left-0 right-0 w-full justify-start">
-        <div className="absolute left-0 -top-[70px] text-left">
+        <div className="absolute left-0 -top-[70px] text-left -z-10">
           <Image src={"/sallad1.png"} width={109} height={189} alt={"sallad"} />
         </div>
-        <div className="absolute -top-[300px] right-0 -z-10">
+        <div className="absolute right-0 -top-[100px] text-right -z-10">
           <Image src={"/sallad2.png"} width={107} height={195} alt={"sallad"} />
         </div>
       </div>
--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -1,7 +1,13 @@
 export default function MenuItem() {
   return (
-    <div className="bg-gray-200 p-4 rounded-lg text-center hover:bg-white transition-all">
-      <img src="/pizza.png" alt="pizza" />
+    <div className="bg-gray-200 p-4 rounded-lg text-center hover:bg-white hover:shadow-md hover:shadow-black/25 transition-all">
+      <div className="text-center">
+        <img
+          src="/pizza.png"
+          className="max-h-auto max-h-24 block mx-auto"
+          alt="pizza"
+        />
+      </div>
       <h4 className="font-semibold text-xl my-3">Pepperoni Pizza</h4>
       <p className="text-gray-500 text-sm">
         Lorem Ipsum is simply dummy text of the printing and typesetting
