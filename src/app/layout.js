1d0555927c5f8b912c68ce658516ceebaf7fbc38
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-02-06 11:05:49 -0600

    Create register

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Header from "@/components/layout/Header";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 
@@ -12,7 +13,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <main className="max-w-4xl mx-auto p-4">{children}</main>
+        <main className="max-w-4xl mx-auto p-4">
+          <Header />
+          {children}
+          <footer className="border-t p-8 text-center text-gray-500 mt-16">
+            &copy; 2024 All rights reserved
+          </footer>
+        </main>
       </body>
     </html>
   );
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,3 +1,7 @@
 export default function RegisterPage() {
-  return <div>register</div>;
+  return (
+    <section className="mt-8">
+      <h1 className="text-center text-primary text-4xl">Register</h1>
+    </section>
+  );
 }
