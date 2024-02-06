f58bcd64cfa550a646bb218d0d101e2fc5b0254e
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-02-05 18:26:22 -0600

    Add other section in the main page

--- src/app/page.js
+++ src/app/page.js
@@ -11,12 +11,32 @@ export default function Home() {
       <HomeMenu />
       <section className="text-center my-6">
         <SectionHeaders subHeader={"Our story"} mainHeader={"About us"} />
+        <div className="max-w-md mx-auto text-gray-500 mt-4 flex flex-col gap-4">
+          <p>
+            Nisi laboris elit proident Lorem duis id tempor esse irure cillum
+            ea. Mollit ipsum in reprehenderit velit magna dolor. Aute
+            reprehenderit excepteur sit sunt elit eu commodo in commodo eiusmod.
+          </p>
+          <p>
+            Fugiat anim eu consequat exercitation aliqua cupidatat pariatur
+            magna. Labore et proident ex eiusmod magna consectetur sit.
+          </p>
+          <p>
+            Qui sunt veniam sint Lorem. Laboris ea ea ad deserunt culpa do in id
+            ullamco labore qui duis enim.
+          </p>
+        </div>
       </section>
-      <p className="max-w-2xl mx-auto mt-4 text-gray-500">
-        Nisi laboris elit proident Lorem duis id tempor esse irure cillum ea.
-        Mollit ipsum in reprehenderit velit magna dolor. Aute reprehenderit
-        excepteur sit sunt elit eu commodo in commodo eiusmod.
-      </p>
+      <section className="text-center my-8">
+        <SectionHeaders
+          subHeader={"Don't hesitate"}
+          mainHeader={"Contact us"}
+        />
+        <a className="text-2xl" href="tel+46748374820">
+          +46 748 374 820
+        </a>
+        <h1></h1>
+      </section>
     </>
   );
 }
