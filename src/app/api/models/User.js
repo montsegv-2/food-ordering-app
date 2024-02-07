e1d2f6210ffe203a893fc71cc69fca7a60ec1ce4
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-02-06 23:49:31 -0600

    Add MongooDB queries with mongoose

--- src/app/api/models/User.js
+++ src/app/api/models/User.js
@@ -1,6 +1,19 @@
 const { Schema } = require("mongoose");
 
-const UserSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: string, required: true, validate: () => {} },
-});
+const UserSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: {
+      type: string,
+      required: true,
+      validate: (pass) => {
+        if (!pass?.length || pass.length < 5) {
+          new Error("password must be at least 5 characters");
+        }
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = models?.User || model("User", UserSchema);
